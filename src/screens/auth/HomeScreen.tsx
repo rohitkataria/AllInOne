9ee3878495ed203446666cac7ca74009631c458d
rohitkataria <rohitkataria0143@gmail.com>
2021-08-04 08:42:30 +0530

    mobx with rootstore

--- src/screens/auth/HomeScreen.tsx
+++ src/screens/auth/HomeScreen.tsx
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Button, Text, View } from 'react-native';
 import { AllNavParamList } from '../../routes/AppRoutesList';
+import { inject, observer } from 'mobx-react';
+import { UserStore } from '../../stores';
+
 
 type HomeScreenNavigationProp = StackNavigationProp<AllNavParamList, 'HomeScreen'>;
 type HomeScreenProps = {
     navigation: HomeScreenNavigationProp;
+    userStore: UserStore;
 };
 
+@inject('userStore')
+@observer
 class HomeScreen extends Component<HomeScreenProps> {
     render() {
-        const { navigation } = this.props
+        const { navigation, userStore } = this.props
         const data = 'badal'
+        userStore.setEmail('sohal@gmail.com');
         return (
             <View>
                 <Text>Home screen</Text>
